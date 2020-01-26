7357bc2eb2bed636b4030dc5e7e26be045366a11
Eduardo Perez <edujpp1@gmail.com>
2020-01-26 18:21:32 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
