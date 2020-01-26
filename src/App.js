62ab3435934525f42e2009bf1ec9f5a44e0bb93f
Eduardo Perez <edujpp1@gmail.com>
2020-01-26 20:02:47 +0000

    The Productos, NuevoProducto and EditarProducto components were created. The Header component was modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './componentes/Header';
+import Productos from './componentes/Productos';
+import NuevoProducto from './componentes/NuevoProducto';
+import EditarProducto from './componentes/EditarProducto';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -8,9 +12,18 @@ import store from './store';
 function App() {
   return (
     <Provider store={store}>
-      <React.Fragment>
-        <Header />
-      </React.Fragment>
+      <Router>
+        <React.Fragment>
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Productos}/>
+              <Route exact path="/productos/nuevo" component={NuevoProducto}/>
+              <Route exact path="/productos/editar/:id" component={EditarProducto}/>
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
     </Provider>
   );
 }
---
+++ src/componentes/NuevoProducto.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class NuevoProducto extends Component {
+  render() {
+    return (
+      <h1>Desde NuevoProducto</h1>
+    )
+  }
+}
+
+export default NuevoProducto;
