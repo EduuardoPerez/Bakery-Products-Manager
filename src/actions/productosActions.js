9d6d3886812e7a8947c47c851ec87c17bd6a6a81
Eduardo Perez <edujpp1@gmail.com>
2020-01-27 15:03:04 +0000

    A new product can be created after validate the form on NuevoProducto component. After the product is created the app redirect to home

--- src/actions/productosActions.js
+++ src/actions/productosActions.js
@@ -1,4 +1,4 @@
-import { MOSTRAR_PRODUCTOS, ELIMINAR_PRODUCTO } from './types';
+import { MOSTRAR_PRODUCTOS, ELIMINAR_PRODUCTO, AGREGAR_PRODUCTO } from './types';
 import axios from 'axios';
 
 //  When working with APIs and reducers it's in the actions where the API call is made
@@ -16,5 +16,13 @@ export const borrarProducto = id => async (dispatch) => {
   dispatch({
     type: ELIMINAR_PRODUCTO,
     payload: id
+  })
+}
+
+export const agregarProducto = post => async (dispatch) => {
+  const respuesta = await axios.post('http://192.168.0.55:5000/productos', post);
+  dispatch({
+    type: AGREGAR_PRODUCTO,
+    payload: respuesta.data
   })
 }
