b24b4d9db2d7860b2e72f9d5a9ca285bfff68769
Eduardo Perez <edujpp1@gmail.com>
2020-01-27 14:18:40 +0000

    The Producto component was finished

--- src/componentes/Producto.js
+++ src/componentes/Producto.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Producto extends Component {
   state = {  }
   render() {
+
+    const { id, nombre, precio } = this.props.info;
+
     return (
-      <h2>Nombre Producto</h2>
+      <li className="list-group-item">
+        <div className="row justify-content-between align-items-center">
+          <div className="col-md-8 d-flex justify-content-between align-items-center">
+            <p className="text-dark m-0">
+              { nombre }
+            </p>
+            <span className="badge badge-warning text-dark">
+              $ { precio }
+            </span>
+          </div>
+          <div className="col-md-4 d-flex justify-content-end acciones">
+            <Link to={`productos/editar/${id}`} className="btn btn-primary mr-2">Editar</Link>
+            <button type="button" className="btn btn-danger">Borrar</button>
+          </div>
+        </div>
+      </li>
     )
   }
 }
